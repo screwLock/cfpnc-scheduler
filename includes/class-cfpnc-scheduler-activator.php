0244c438cdc4f2c63f5665bcbc5ecba2c82836c8
screwLock <helmlyw@gmail.com>
2018-08-14 14:13:33 -0400

    first commit

---
+++ includes/class-cfpnc-scheduler-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://github.com/screwLock
+ * @since      1.0.0
+ *
+ * @package    Cfpnc_Scheduler
+ * @subpackage Cfpnc_Scheduler/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Cfpnc_Scheduler
+ * @subpackage Cfpnc_Scheduler/includes
+ * @author     Travus Helmly <helmlyw@gmail.com>
+ */
+class Cfpnc_Scheduler_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-cfpnc-scheduler-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://github.com/screwLock
+ * @since      1.0.0
+ *
+ * @package    Cfpnc_Scheduler
+ * @subpackage Cfpnc_Scheduler/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Cfpnc_Scheduler
+ * @subpackage Cfpnc_Scheduler/includes
+ * @author     Travus Helmly <helmlyw@gmail.com>
+ */
+class Cfpnc_Scheduler_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
